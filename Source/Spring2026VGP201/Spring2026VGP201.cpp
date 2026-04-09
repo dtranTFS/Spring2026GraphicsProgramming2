17c3a6b749666a7b860d23f940e862fff28434ad
Danny Tran <dtran@torontofilmschool.ca>
2026-04-09 19:06:30 -0400

    Inital Project
    Tested compiled using live code and made a basic game mode

---
+++ Source/Spring2026VGP201/Spring2026VGP201.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Spring2026VGP201.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Spring2026VGP201, "Spring2026VGP201" );
